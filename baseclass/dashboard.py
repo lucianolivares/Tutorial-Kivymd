61d9277e7cf7ff307a6b3319956ae9c0dfa15167
Luciano Olivares López <50638090+lucianolivares@users.noreply.github.com>
2020-05-01 18:46:17 -0400

    Add files branch Tutorial3

--- baseclass/dashboard.py
+++ baseclass/dashboard.py
@@ -1,6 +1,8 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 
+from baseclass.banner import Banner
+
 
 class DashBoard(Screen):
     def __init__(self, **kw):
@@ -9,8 +11,20 @@ class DashBoard(Screen):
         self.sub_title = "Convertidor de números binarios"
         self.hint_binary_number = "Ingresar un número binario"
 
-    def on_enter(self, *args):
+    def on_pre_enter(self, *args):
         self.app.title = "Dash Board"
+
+    def on_kv_post(self, base_widget):
+        grid = self.ids["grid_banner"]
+        operations = {"binary": "Convertidor de Números Binario",
+                      "count_vowels": "Contador de Vocales",
+                      "is_leap": "Comprobar Año Bisiesto",
+                      "is_palindrome": "Verificador de Palíndromo",
+                      "inverse": "Invertir Sentencia"
+                      }
+        for operation, title in operations.items():
+            banner = Banner(title=title, operation=operation)
+            grid.add_widget(banner)
 
     def is_binary(self, binary_number):
         try:
