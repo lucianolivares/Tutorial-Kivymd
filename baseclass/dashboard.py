4a2a17bde9d3a69e71fd2f9dbf780fef196bd6c9
Luciano Olivares López <50638090+lucianolivares@users.noreply.github.com>
2020-06-13 16:11:31 -0400

    Add files Tutorial 8
    Add screen in main.py with a dictionary

--- baseclass/dashboard.py
+++ baseclass/dashboard.py
@@ -1,28 +1,18 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 
-from baseclass.banner import Banner
+from baseclass.classes import Banner
 
 
 class DashBoard(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.app = MDApp.get_running_app()
-
-    def on_pre_enter(self, *args):
-        self.app.title = "Dash Board"
-
     def on_kv_post(self, base_widget):
-        # Rellenar ScrollView con banners
         grid = self.ids["grid_banner"]
-        # Indicamos la operación y el título
         operations = {"binary": "Convertidor de Números Binario",
                       "count_vowels": "Contador de Vocales",
                       "is_leap": "Comprobar Año Bisiesto",
                       "is_palindrome": "Verificador de Palíndromo",
                       "inverse": "Invertir Sentencia"
                       }
-
         for operation, title in operations.items():
-            banner = Banner(operation=operation, title=title)
+            banner = Banner(title=title, operation=operation)
             grid.add_widget(banner)
--- main.py
+++ main.py
@@ -1,19 +1,54 @@
+# Importar librerías
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from functools import partial
+import os.path
 
 
+# Importar Pantallas
 from baseclass.dashboard import DashBoard
 from baseclass.settingsscreen import SettingsScreen
 from baseclass.pickersscreen import PickersScreen
 from baseclass.suggestionsscreen import SuggestionsScreen
+from baseclass.classes import ListIcon
 
 
+# Inicialización de la APP
 class MyApp(MDApp):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        # Diccionario de pantallas, Pantalla: (id, nombre a mostrar ("text"), icono)
+        self.list_screen = {
+            DashBoard: ("dashboard", "DashBoard", "view-dashboard"),
+            PickersScreen: ("pickers_screen", "Pickers", "calendar-edit"),
+            SuggestionsScreen: ("suggestions_screen", "Sugerencias", "account-card-details"),
+            SettingsScreen: ("settings_screen", "Ajustes", "settings")
+        }
+
+    # Constructor
     def build(self):
-        self.title = ""
+        # Ajustes adicionales
+        self.title = "Mi Primera App"
         self.theme_cls.primary_palette = "Green"
-        return Builder.load_file("main.kv")
+        # Carga del archivo kivy
+        return Builder.load_file('main.kv')
+
+    def on_start(self):
+        for screen, details in self.list_screen.items():
+            identification, text, icon = details
+            kv_file = identification.replace("_", "")
+            Builder.load_file(f"kv/{kv_file}.kv")
+            self.root.ids.screen_manager.add_widget(screen(name=identification))
+            self.root.ids.nav_list.add_widget(ListIcon(text=text, icon=icon,
+                                                       on_release=partial(self.button_list_actions,
+                                                                          text,
+                                                                          identification)))
+
+    def button_list_actions(self, nombre, identification):
+        self.title = nombre
+        self.root.ids.screen_manager.current = identification
+        self.root.ids.nav_drawer.set_state()
 
 
-MyApp().run()
+if __name__ in ('__main__', '__android__'):
+    MyApp().run()
